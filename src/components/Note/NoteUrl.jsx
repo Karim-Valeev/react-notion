5bd66143c58175b8404acb62693752240cd8807c
Karim-Valeev <karim.valeev.i@gmail.com>
2022-01-23 04:51:25 +0300

    Apply prettier

--- src/components/Note/NoteUrl.jsx
+++ src/components/Note/NoteUrl.jsx
@@ -1,18 +1,18 @@
-import {Link} from "react-router-dom";
-function NoteUrl ({noteUrl}) {
-
-    function shortTitle (value) {
-        return value.length > 15 ? `${value.substr(0, 14)}...` : value
+import { Link } from 'react-router-dom';
+function NoteUrl({ noteUrl }) {
+    function shortTitle(value) {
+        return value.length > 15 ? `${value.substr(0, 14)}...` : value;
     }
 
- const listItems =   noteUrl.map((item, index) =>
-     <div key={index}>
-      <Link to={`/note/${item.id}`} className="url__item">{shortTitle(item.title)}</Link>
-      {(index !== noteUrl.length - 1) ?  <span className="line__oblique">/</span> : ''}
-     </div>)
- return  <nav className="notion__url">
-  { listItems }
- </nav>
+    const listItems = noteUrl.map((item, index) => (
+        <div key={index}>
+            <Link to={`/note/${item.id}`} className="url__item">
+                {shortTitle(item.title)}
+            </Link>
+            {index !== noteUrl.length - 1 ? <span className="line__oblique">/</span> : ''}
+        </div>
+    ));
+    return <nav className="notion__url">{listItems}</nav>;
 }
 
 export default NoteUrl;
--- src/containers/Sidebar/NotionListContainer.jsx
+++ src/containers/Sidebar/NotionListContainer.jsx
@@ -5,21 +5,20 @@ import {
     handleAddNote as handleAddNoteDispatch,
     handleDeleteNote,
     handleNotionList,
-} from "../../store/actions/NotionListActions";
-import * as notionListSelectors from "../../store/selectors/notion_list_selectors"
-import * as userSelectors from "../../store/selectors/user_selectors"
-import * as noteSelectors from "../../store/selectors/note_selectors"
-import LoaderNotionList from "../../components/Loaders/LoaderNotionList";
-import {useHistory} from "react-router-dom";
+} from '../../store/actions/NotionListActions';
+import * as notionListSelectors from '../../store/selectors/notion_list_selectors';
+import * as userSelectors from '../../store/selectors/user_selectors';
+import * as noteSelectors from '../../store/selectors/note_selectors';
+import LoaderNotionList from '../../components/Loaders/LoaderNotionList';
+import { useHistory } from 'react-router-dom';
 
-
-function NotionListContainer () {
-    const notionList = useSelector(notionListSelectors.notionList)
-    const note = useSelector(noteSelectors.note)
-    const load = useSelector(notionListSelectors.load)
-    const user = useSelector(userSelectors.user)
-    const dispatch = useDispatch()
-    const history = useHistory()
+function NotionListContainer() {
+    const notionList = useSelector(notionListSelectors.notionList);
+    const note = useSelector(noteSelectors.note);
+    const load = useSelector(notionListSelectors.load);
+    const user = useSelector(userSelectors.user);
+    const dispatch = useDispatch();
+    const history = useHistory();
 
     useEffect(() => {
         dispatch(handleNotionList(user));
@@ -37,13 +36,17 @@ function NotionListContainer () {
     };
 
     const handleDelete = (data) => {
-        dispatch(handleDeleteNote([{...data, author: user.uid}]))
+        dispatch(handleDeleteNote([{ ...data, author: user.uid }]));
         if (note?.id === data.id) {
-            history.push('/')
+            history.push('/');
         }
-    }
+    };
 
-    return (load) ?  <LoaderNotionList/> : <NotionListItem notionList={notionList} note={note} handleAddNote={handleAddNote} handleDelete={handleDelete}/>
+    return load ? (
+        <LoaderNotionList />
+    ) : (
+        <NotionListItem notionList={notionList} note={note} handleAddNote={handleAddNote} handleDelete={handleDelete} />
+    );
 }
 
 export default NotionListContainer;
